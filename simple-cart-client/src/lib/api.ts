ddd54cbed040a1cce8c21d680d2f02bd9ae51cae
ajayahara <abak00350@gmail.com>
2025-10-05 11:33:44 +0530

    clint: ready to deploy

--- simple-cart-client/src/lib/api.ts
+++ simple-cart-client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import type { ICheckoutRequest, ICheckoutResponse, IProduct } from "@/types";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = {
   async getProducts(): Promise<IProduct[]> {
